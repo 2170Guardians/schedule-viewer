0085539ad4a6e6dba81e923389d0a24da4c2e8e4
Farhan Kittur <galvanizedg2170@gmail.com>
2025-07-11 02:11:31 -0400

    small updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import './App.css';
 
-function RoundRect({title, ID, timing}) {
+function RoundRect({title, ID, timing, section, instr, loc, seats}) {
   return (
     <div className = 'block'>
-      <p className="block-header">{title}</p>
-      <div className='row'>
-        <p>{ID}</p>
+      
+      
+      <div className='row spread'> 
+        <p className="block-header">{ID}</p>
         <p>{timing}</p>
       </div>
+
+      <p className = "block-subheader">{title}</p>
       
     </div>
   );
+}
+
+class TimeBlock {
+  constructor() {
+    this.ID = "CMSC132";
+    this.title = "Object-Oriented Programming II";
+    this.timing = "9:00-9:50AM";
+    this.section = "0303";
+    this.instr = "Nora Burkhauser";
+    this.loc = "CSI 3120";
+    this.seats = 50;
+  }
+  static timingToMinutes(hours, minutes) {
+    return hours * 60 + minutes;
+  }
+
+  static minutesToTiming(minutes) {
+    return [minutes / 60, minutes % 60]
+  }
+}
+
+function column() {
+
 }
 
 
@@ -18,7 +44,7 @@ export default function App() {
   return (
     <>
       <div className="App">
-        <RoundRect title = "DS" ID = "CMSC131" timing = "1PM-3PM" />
+        <RoundRect title = "Object-Oriented Programming II" ID = "CMSC132" timing = "9:00-9:50AM" section = "0303" instr = "Nora Burkhauser" loc = "CSI 3120" seats = {50}/>
       </div>
     </>
   );
